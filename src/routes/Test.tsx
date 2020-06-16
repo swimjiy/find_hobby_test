495a937536c016606c1ec9ded98b6c0c2d47422d
swimjiy <swimjiy@gmail.com>
2020-06-16 22:35:40 +0900

    결과 페이지 전환 전 로딩 추가

--- src/routes/Test.tsx
+++ src/routes/Test.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import { useState } from 'react'
 import Question from '../components/Question';
 import Result from '../components/Result';
+import Loading from '../components/Loading'
 
 const Test = () => {
 	const [type, setType] = useState<number>(0);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const getType = (type: number):void => {
 		setType(type);
+		setTimeout(() => setIsLoading(true), 2000);
 	}
 	return (
 		<section>
@@ -14,7 +17,11 @@ const Test = () => {
 				(type == 0) ? (
 					<Question getType={(type) => getType(type)}/>
 				) : (
-					<Result type={type}/>
+						(!isLoading) ? (
+							<Loading/>
+						) : (
+							<Result type={type}/>
+					)
 				)
 			}
 		</section>
