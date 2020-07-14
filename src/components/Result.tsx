4d7dbadf0628670b794b640078f0a1bdc0125099
swimjiy <swimjiy@gmail.com>
2020-07-14 22:59:26 +0900

    useRef import 삭제

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useEffect, useCallback, useRef } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom';
 import CopyToClipboard from 'react-copy-to-clipboard'
 import QuestionResult from '../data/QuestionResult.json'
